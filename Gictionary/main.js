bb470c70af01bae1905cca3dc25cc50212e84b07
HAMA <29620943+hamawaree@users.noreply.github.com>
2023-01-11 16:53:12 +0900

    検索までできた

--- Gictionary/main.js
+++ Gictionary/main.js
@@ -9,11 +9,34 @@ window.addEventListener('load', () => {
         },
         methods: {
             search(){
-                // alert(dictionary[1].title);
 
-                for(i=0;i<5;i++){
-                    this.result[i] = dictionary[i].title;
+                // let filtered = dictionary.filter(word => word.title.length > 10);
+                let regex = new RegExp(this.textInput);
+                let filteredRoman
+
+                dictionary.forEach((obj)=>{
+                    obj.roman_found = obj.roman.find(arr => arr.match(regex))
+                    if(obj.roman_found){
+                        console.log(obj);
+                    }
+                });
+
+                let filtered = dictionary.filter(obj => obj.roman_found);
+                // let filtered = fiter_befor.filter(word => word.roman.match(regex));
+                // let filtered = dictionary.filter(word => word.title.match(regex));
+
+                console.log(filtered);
+
+                if (filtered.length <= 10){
+                    for(i=0;i<filtered.length;i++){
+                        this.result[i] = filtered[i].title;
+                    }
+                }else{
+                    for(i=0;i<10;i++){
+                        this.result[i] = filtered[i].title;
+                    }
                 }
+
                 console.log(this.result);
             }
         }
