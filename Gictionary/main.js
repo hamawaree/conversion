1617e733c3cdf2b834760a493783074203328976
HAMA <29620943+hamawaree@users.noreply.github.com>
2023-01-16 05:39:55 +0900

    速記記法を検索可能にした

--- Gictionary/main.js
+++ Gictionary/main.js
@@ -4,7 +4,9 @@ window.addEventListener('load', () => {
             return{
                 textInput: "",
                 result:[],
-                dl_result: []
+                dl_result: [],
+                sokki_result: [],
+                regex_trace:""
             }
         },
         methods: {
@@ -45,6 +47,31 @@ window.addEventListener('load', () => {
                 // レーベンシュタインの結果を格納
                 for(i=0;i<filtered.length;i++){
                     this.dl_result[i] = filtered[i].title;
+                }
+            },
+            sokkiSearch(){
+                let regex_befor = ""
+                for(i=0;i<this.textInput.length;i++){
+                    regex_befor += ".*"
+                    regex_befor += this.textInput[i]
+                }
+                regex_befor += ".*"
+
+                this.regex_trace = regex_befor;
+                let regex = new RegExp(regex_befor);
+
+                dictionary.forEach((obj)=>{
+                    obj.sokki_found = obj.roman.find(arr =>arr.match(regex))
+                });
+
+                let filtered = dictionary.filter(obj => obj.sokki_found);
+                
+                // 前の検索結果を削除
+                this.sokki_result.splice(0);
+
+                // レーベンシュタインの結果を格納
+                for(i=0;i<filtered.length;i++){
+                    this.sokki_result[i] = filtered[i].title;
                 }
             }
         }
