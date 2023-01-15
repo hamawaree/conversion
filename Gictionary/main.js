49917dcf14cd7f107d470a31d8f95aa34c59058e
HAMA <29620943+hamawaree@users.noreply.github.com>
2023-01-15 20:49:14 +0900

    ダメラウレーベンシュタイン距離の結果も表示するようにした

--- Gictionary/main.js
+++ Gictionary/main.js
@@ -1,18 +1,18 @@
 window.addEventListener('load', () => {
-
     const appdata = {
         data() {
             return{
                 textInput: "",
-                result:[]
+                result:[],
+                dl_result: []
             }
         },
         methods: {
             search(){
                 let regex = new RegExp(this.textInput);
-
+ 
                 dictionary.forEach((obj)=>{
-                    obj.roman_found = obj.roman.find(arr => arr.match(regex))
+                    obj.roman_found = obj.roman.find(arr =>arr.match(regex))
                 });
 
                 let filtered = dictionary.filter(obj => obj.roman_found);
@@ -30,8 +30,22 @@ window.addEventListener('load', () => {
                         this.result[i] = filtered[i].title;
                     }
                 }
+            },
+            dlTest(){
+                console.log(distance(this.textInput, "sato"));
+                dictionary.forEach((obj)=>{
+                    obj.dl_found = obj.roman.find(word => distance(this.textInput,word) <= 1)
+                });
 
-                console.log(this.result);
+                let filtered = dictionary.filter(obj => obj.dl_found);
+
+                // 前の検索結果を削除
+                this.dl_result.splice(0)
+
+                // レーベンシュタインの結果を格納
+                for(i=0;i<filtered.length;i++){
+                    this.dl_result[i] = filtered[i].title;
+                }
             }
         }
     }
